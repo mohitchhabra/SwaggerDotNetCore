a3459ad0d89373075819caf5dc70a187d009ce69
Glenn Condron <glennc@microsoft.com>
2018-09-11 19:12:37 -0700

    Initial app

---
+++ SwaggerApp/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public static List<Person> People = new List<Person>();
+
+        public PeopleController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            return People.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(int id)
+        {
+            return People.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Person person)
+        {
+            People.Add(person);
+            return Ok();
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
